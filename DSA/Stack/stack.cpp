61e4cafca22f38ef89c8ce13fe1eebca66ed857a
Samir Shrestha <ss1366t@gre.ac.uk>
2024-04-16 16:40:21 +0100

    basics of stack

---
+++ DSA/Stack/stack.cpp
@@ -0,0 +1,33 @@
+//basic structure of stack
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void printStackElement(stack<int>stack) {
+
+    cout << "The size of stack is :" << stack.size() << endl;
+
+    while (!stack.empty()) {
+        cout << stack.top() << endl;
+        stack.pop();
+    }
+}
+
+int main() {
+    stack<int>numberstack;  // definning stack to hold integer
+
+    numberstack.push(1); //inserts integer to the stack
+    numberstack.push(2);
+    numberstack.push(3);
+    numberstack.push(4);
+
+    if (numberstack.empty()) {  //checks if the stack is empty
+        cout << "stack is empty";
+    }
+    else {
+        printStackElement(numberstack);
+    }
+    return 0;
+}
