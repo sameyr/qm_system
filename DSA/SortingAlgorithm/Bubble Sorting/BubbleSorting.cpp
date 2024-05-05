157ad97dd86e0a7df97c0673cf32510331c94755
Samir Shrestha <ss1366t@gre.ac.uk>
2024-05-05 18:44:07 +0100

    selection sorting added

--- DSA/SortingAlgorithm/Bubble Sorting/BubbleSorting.cpp
+++ DSA/SortingAlgorithm/Bubble Sorting/BubbleSorting.cpp
@@ -8,14 +8,17 @@ int main(){
     int size = sizeof(array1)/sizeof(array1[0]);
     bool swapped; 
 
+    //prints unsorted array
     cout << "Unsorted Array: ";
     for(int i =0 ; i < size ; i++){
         cout << array1[i] << " ";
     } 
     cout <<endl;
 
+    //loop to initiate bubble sorting
     for(int i =0 ; i < (size - 1) ; i++){
         swapped = false;
+        
         for(int j =0 ; j < (size-i-1) ; j++){
             
             if (array1[j] > array1[j+1]){
@@ -26,11 +29,13 @@ int main(){
             }
             
         }
+
         if(swapped==false){
                 break;
         }
     }
 
+    //prints sorted array
     cout << "Sorted Array: ";
     for(int i =0 ; i < size ; i++){
         cout << array1[i] << " ";
