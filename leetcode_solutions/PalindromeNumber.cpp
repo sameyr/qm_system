bf9cd164fa492cc7f390d780f95b5aaff474f0b6
Samir Shrestha <ss1366t@gre.ac.uk>
2024-05-06 15:16:52 +0100

    more description added for palindrome solution from leetcode: difficulty level: easy

--- leetcode_solutions/PalindromeNumber.cpp
+++ leetcode_solutions/PalindromeNumber.cpp
@@ -1,3 +1,27 @@
+/*
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+ */
+
+#include <iostream>
+
+using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
@@ -20,3 +44,10 @@ public:
         }
     }
 };
+
+int main(){
+    int num = 2442;
+    Solution s1;
+
+    cout <<"The give number '"<< num << "' is palindrome: " << s1.isPalindrome(num) << endl;
+}
