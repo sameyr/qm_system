1a869fc58bbbeed2514f1dd460002c75237a2d5c
Samir Shrestha <ss1366t@gre.ac.uk>
2024-05-15 17:50:03 +0100

    longest prefix question. not solved: difficulty level : Easy.

---
+++ leetcode_solutions/longestPrefix.cpp
@@ -0,0 +1,78 @@
+/* Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int len = strs.size();
+        string op = "";
+        string final_op = "";
+        string first = strs[0];
+        int first_length = first.length(); 
+        string temp = strs[1];
+        int temp_length = temp.length();
+        int j=0, k=0;
+
+        while(j < first_length && k < temp_length){
+            if (first[j] == temp[k] ){
+                op += first[j];
+                j++;
+                k++;
+            }
+            else if (k < temp_length){
+                k++;
+            }
+            else if ((k >= temp_length) && (j < first_length)){
+                j++;
+            }
+            else{
+                return "";
+            }
+        }
+
+        /*for(int i =2 ; i < len ; i++){
+            temp = strs[i];
+            temp_length = temp.length();
+            j=0;
+            while (j < temp_length){
+                if(op[0] == temp[j]){
+                    final_op = op[0];
+                    j++;
+                    op.erase(0,1);
+                }
+                else{
+                    op.erase(0,1);
+                }
+            }
+            op = final_op;
+        }*/
+        return op;
+    }
+};
+
+int main(){
+    vector<string> str  = {"samir", "srarm", "laudu","khatey"};
+    string answer;
+    Solution s1;
+
+    answer = s1.longestCommonPrefix(str);
+    cout << answer;
+}
