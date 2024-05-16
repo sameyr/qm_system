1f704c4880ca742747af4ae8f1634a2adbae1245
Samir Shrestha <ss1366t@gre.ac.uk>
2024-05-16 20:59:10 +0100

    longest prefix question. solved, help taken from gpt : difficulty level : Easy.

--- leetcode_solutions/longestPrefix.cpp
+++ leetcode_solutions/longestPrefix.cpp
@@ -21,55 +21,33 @@ using namespace std;
 
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
+    string longestCommonPrefix(vector<string>& strs) {        
         int len = strs.size();
-        string op = "";
-        string final_op = "";
-        string first = strs[0];
-        int first_length = first.length(); 
-        string temp = strs[1];
-        int temp_length = temp.length();
-        int j=0, k=0;
-
-        while(j < first_length && k < temp_length){
-            if (first[j] == temp[k] ){
-                op += first[j];
-                j++;
-                k++;
-            }
-            else if (k < temp_length){
-                k++;
-            }
-            else if ((k >= temp_length) && (j < first_length)){
-                j++;
-            }
-            else{
-                return "";
-            }
+        
+        if (len ==0){
+            return "";
+        }
+        else if (len ==1){
+            return strs[0];
         }
 
-        /*for(int i =2 ; i < len ; i++){
-            temp = strs[i];
-            temp_length = temp.length();
-            j=0;
-            while (j < temp_length){
-                if(op[0] == temp[j]){
-                    final_op = op[0];
-                    j++;
-                    op.erase(0,1);
-                }
-                else{
-                    op.erase(0,1);
+        string op = strs[0];
+        string temp;
+
+        for (int i = 1; i < len; i++) {
+            while (strs[i].find(op) != 0) {
+                op.erase(op.size() - 1, 1);
+                if (op.empty()) {
+                    return "";
                 }
             }
-            op = final_op;
-        }*/
+        }
         return op;
     }
 };
 
 int main(){
-    vector<string> str  = {"samir", "srarm", "laudu","khatey"};
+    vector<string> str  = {"sam","sam"};
     string answer;
     Solution s1;
 
