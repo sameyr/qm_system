a72f3dfdb26deaf3747377696280244ee67c1ebf
Samir Shrestha <ss1366t@gre.ac.uk>
2024-09-27 14:41:44 +0100

    windows.h >> copyFile function used.

--- Windows System Programming/copyFile.cpp
+++ Windows System Programming/copyFile.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 int main(){
 
 BOOL bFile;
@@ -12,7 +11,7 @@ bFile = CopyFileA(
         "D:\\personal_file\\learning_stuff\\qm_system\\Windows System Programming\\OldFile.txt",
         "D:\\personal_file\\learning_stuff\\qm_system\\Windows System Programming\\NewFile.txt",
         NULL
-);
+);   // copyFileA finction copies an existing file to a new file
 
 if (bFile == false){
         cout << "Copy Failed & error no is : " << GetLastError() <<endl; 
